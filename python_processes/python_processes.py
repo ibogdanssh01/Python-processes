97fd1af5d8025f9782899b5e491e0b3f0bdb9330
ibogdanssh <ibogdanssh@gmail.com>
2025-06-08 01:53:01 +0300

    Added new methods for processes
    Now you can see all the processes, next step is too create a parent -> childs nodes, in order to put them in a mock DB in that way.

--- python_processes/python_processes.py
+++ python_processes/python_processes.py
@@ -1,44 +1,84 @@
+""" -------------------- HEADERS -------------------- """
+
 from pathlib import Path
 import wmi
 import sys
 sys.path.append('../')
-from gui.guiApp import App
-from dictionary.order import order_by_name
-from duration_logic.calculate_time import getInitTime, getClass, process_time
 from python_processes.dataclasses import ProcessInfo
 from python_processes.enums import ProcessType
 import psutil
 from pathlib import Path
 import orjson
 
-def get_processes() -> dict:
-    processDict = dict()
-    getProcess = wmi.WMI()
-    for process in getProcess.Win32_Process():
-        processDict[process.ProcessId] = process.Name
-    return processDict
 
-def findPath(name):
+""" -------------------- HEADERS -------------------- """
+
+
+
+""" -------------------- FUNCTIONS -------------------- """
+
+def findPath(name: str):
     for pid in psutil.pids():
         if psutil.Process(pid).name() == name:
             return psutil.Process(pid).exe()
 
+def printProcesses(data: dict) -> None:
+    for pid, info in data.items():
+        name        = info.get("name")
+        abs_path    = info.get("abs_path")
+        status      = info.get("status")
 
-# def process_categorizer(process_Name: str, processID: int, time_of_creation: tuple) -> ProcessInfo:
-def process_categorizer(FILE_PATH) -> None:
-    # time_of_creation tuple (0 hour, 0 minute)
-    p = Path(FILE_PATH)
-    data = orjson.loads(p.read_bytes())
-    dict_processes = get_processes()
-    for process_id, process_name in dict_processes.items():
-        process = psutil.Process(process_id)
-        parent_ppid = process.ppid()
-        if parent_ppid not in data.keys():
-            if not process_name.endswith(".exe"):
-                category = ProcessType.PROCESS_CATEGORY_WINDOWS_SYSTEM.value
-            else:
-                category = ProcessType.PROCESS_CATEGORY_OTHER.value
-            data[str(parent_ppid)] = "({}, {})".format(process_name, category)
-        else:
-            error = "PPID EXIST ALREADY IN DICT"
-    p.write_bytes(orjson.dumps(data))
+        print(f"Parent PID: {pid}")
+        print(f"  Name    : {name}")
+        print(f"  Path    : {abs_path}")
+        print(f"  Status  : {status}")
+        print()
+
+def getAllProcesses() -> dict:
+    data = dict()
+
+    for proc in psutil.process_iter(['pid', 'name', 'status']):
+        try:
+            child = proc.info['pid']
+            name = proc.info['name']
+            status = proc.info["status"]
+
+            if child in data:
+                continue
+
+            if child not in data:
+                data[child] = {}
+
+            data[child]["name"]        = name
+            data[child]["status"]      = status
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            continue
+    return data
+
+def getProcessPID() -> dict:
+    data = dict()  # -> store parent pid
+
+    for proc in psutil.process_iter(['pid', 'ppid', 'name', 'exe', 'status']):
+        try:
+            parent      = proc.info['ppid']
+            name        = proc.info['name']
+            abs_path    = proc.info['exe']
+            status      = proc.info['status']
+
+            if parent not in data:
+                data[parent] = {}
+
+            data[parent]["name"]        = name
+            data[parent]["abs_path"]    = abs_path
+            data[parent]["status"]      = status
+
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            continue
+
+    return data
+
+
+def process_categorizer(process_Name: str, processID: int, time_of_creation: tuple) -> ProcessInfo:
+    pass
+
+""" -------------------- FUNCTIONS -------------------- """
