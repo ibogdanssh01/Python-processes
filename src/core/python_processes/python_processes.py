2c899e667c2ad09c20642c42e9761caeac56a780
ibogdanssh <ibogdanssh@gmail.com>
2025-06-17 17:47:30 +0300

    Code refactoring, adding classes

--- src/core/python_processes/python_processes.py
+++ src/core/python_processes/python_processes.py
@@ -1,14 +1,10 @@
 """ -------------------- HEADERS -------------------- """
 
 import sys
-import os
-import time
 sys.path.append('../')
-from pathlib import Path
 from src.core.python_processes.enums import ProcessType
 from src.config.json_script import rule_loader
 import psutil
-import orjson
 from datetime import datetime
 
 """ -------------------- HEADERS -------------------- """
@@ -21,89 +17,106 @@ rules = rule_loader()
 
 """ -------------------- FUNCTIONS -------------------- """
 
-def getProcessesWithParent() -> dict:
-    data = {}
-    parent_name_cache = {}
+class ProcessMonitor:
+    def __init__(self):
+        self.parent_name_cache: dict[int, str] = {}
 
-    for proc in psutil.process_iter(attrs=['pid', 'ppid', 'name', 'status', 'exe']):
-        try:
-            info = proc.info
-            ppid = info['ppid']
+    def get_processes_with_parents(self) -> dict[str, dict[str, dict]]:
+        data: dict[str, dict[str, dict]] = {}
 
-            if ppid not in parent_name_cache:
-                try:
-                    parent_name_cache[ppid] = psutil.Process(ppid).name().removesuffix('.exe')
-                except (psutil.NoSuchProcess, psutil.AccessDenied):
-                    parent_name_cache[ppid] = "unknown"
-
-            parent_name = parent_name_cache[ppid]
-            parent_key = f"parent_{ppid}_{parent_name}"
-            child_key = f"child_{info['pid']}"
-            parent_dict = data.setdefault(parent_key, {})
-            parent_dict[child_key] = {
-                "name": info['name'],
-                "status": info['status'],
-                "path": info['exe'],
-                "process_type": get_process_category(info['exe'])
-            }
-        except (psutil.NoSuchProcess, psutil.AccessDenied, KeyError):
-            continue
-
-    return data
-
-
-def display_process_tree(process_dict, indent=0):
-    for parent_pid, children in process_dict.items():
-        print("    " * indent + f"{parent_pid}: {{")
-        for child_pid, info in children.items():
-            print("    " * (indent + 1) + f"{child_pid}: {{")
-            for key, value in info.items():
-                print("    " * (indent + 2) + f'"{key}": {repr(value)}')
-            print("    " * (indent + 1) + "},")
-        print("    " * indent + "},")
-
-def monitor_system_to_json(log_interval=2, duration=10, output_path='./output/process_log.json'):
-    log_data = []
-    start_time = time.time()
-
-    while time.time() - start_time < duration:
-        timestamp = datetime.now().isformat()
-        snapshot = []
-
-        for proc in psutil.process_iter(['pid', 'name','cpu_percent', 'memory_info']):
+        for proc in psutil.process_iter(attrs=['pid', 'ppid', 'name', 'status', 'exe']):
             try:
-                cpu = proc.cpu_percent(interval=0)
-                mem = proc.info['memory_info'].rss / (1024 * 1024) # MB
-                snapshot.append({
-                    'pid': proc.info['pid'],
-                    'name': proc.info['name'],
-                    'cpu_percent': round(cpu, 2),
-                    'memory_mb': round(mem, 2)
-                })
-            except (psutil.NoSuchProcess, psutil.AccessDenied):
+                info = proc.info
+                parent_key = self._get_parent_key(info['ppid'])
+
+                child_key = f"child_{info['pid']}"
+                parent_dict = data.setdefault(parent_key, {})
+                parent_dict[child_key] = {
+                    "name": info['name'],
+                    "status": info['status'],
+                    "path": info['exe'],
+                    "process_type": self._get_process_category(info['exe'])
+                }
+            except (psutil.NoSuchProcess, psutil.AccessDenied, KeyError):
                 continue
-        log_data.append({
-            'timestamp': timestamp,
-            'processes': snapshot
-        })
-
-        time.sleep(log_interval)
-        with open(output_path, 'wb') as f:
-            f.write(orjson.dumps(log_data))
+        return data
 
 
+    def _get_parent_key(self, ppid: int) -> str:
+        if ppid not in self.parent_name_cache:
+            try:
+                name = psutil.Process(ppid).name()
+                self.parent_name_cache[ppid] = name.removesuffix(".exe")
+            except (psutil.NoSuchProcess, psutil.AccessDenied):
+                self.parent_name_cache[ppid] = "unknown"
+        return f"parent_{ppid}_{self.parent_name_cache[ppid]}"
 
-def get_process_category(path: str) -> ProcessType:
-    if not path:
-        return ProcessType.PROCESS_CATEGORY_SYSTEM
-
-    path = path.lower()
-
-    for sys_path in rules["system_paths"]:
-        if sys_path in path:
+    def _get_process_category(self, path: str) -> ProcessType:
+        if not path:
             return ProcessType.PROCESS_CATEGORY_SYSTEM
 
-    return ProcessType.PROCESS_CATEGORY_APP
+        path = path.lower()
+        for sys_path in rules["system_paths"]:
+            if sys_path in path:
+                return ProcessType.PROCESS_CATEGORY_SYSTEM
+
+        return ProcessType.PROCESS_CATEGORY_APP
+
+# ! TEST PURPOSE ONLY
+# def display_process_tree(process_dict, indent=0):
+#     for parent_pid, children in process_dict.items():
+#         print("    " * indent + f"{parent_pid}: {{")
+#         for child_pid, info in children.items():
+#             print("    " * (indent + 1) + f"{child_pid}: {{")
+#             for key, value in info.items():
+#                 print("    " * (indent + 2) + f'"{key}": {repr(value)}')
+#             print("    " * (indent + 1) + "},")
+#         print("    " * indent + "},")
+
+
+# ! TEST PURPOSE ONLY -> TODO: In this moment, in near future after a few optimization can be processes like a new feature
+# def monitor_system_to_json(log_interval=2, duration=10, output_path='./output/process_log.json'):
+#     log_data = []
+#     start_time = time.time()
+
+#     while time.time() - start_time < duration:
+#         timestamp = datetime.now().isformat()
+#         snapshot = []
+
+#         for proc in psutil.process_iter(['pid', 'name','cpu_percent', 'memory_info']):
+#             try:
+#                 cpu = proc.cpu_percent(interval=0)
+#                 mem = proc.info['memory_info'].rss / (1024 * 1024) # MB
+#                 snapshot.append({
+#                     'pid': proc.info['pid'],
+#                     'name': proc.info['name'],
+#                     'cpu_percent': round(cpu, 2),
+#                     'memory_mb': round(mem, 2)
+#                 })
+#             except (psutil.NoSuchProcess, psutil.AccessDenied):
+#                 continue
+#         log_data.append({
+#             'timestamp': timestamp,
+#             'processes': snapshot
+#         })
+
+#         time.sleep(log_interval)
+#         with open(output_path, 'wb') as f:
+#             f.write(orjson.dumps(log_data))
+
+
+
+# def get_process_category(path: str) -> ProcessType:
+#     if not path:
+#         return ProcessType.PROCESS_CATEGORY_SYSTEM
+
+#     path = path.lower()
+
+#     for sys_path in rules["system_paths"]:
+#         if sys_path in path:
+#             return ProcessType.PROCESS_CATEGORY_SYSTEM
+
+#     return ProcessType.PROCESS_CATEGORY_APP
 
 
 """ -------------------- FUNCTIONS -------------------- """
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 """ -------------------- HEADERS -------------------- """
 
-from src.core.python_processes.python_processes import getProcessesWithParent
+from src.core.python_processes.python_processes import ProcessMonitor
 from src.config.json_script import dump_data
 from time import perf_counter
 """ -------------------- HEADERS -------------------- """
@@ -8,6 +8,7 @@ from time import perf_counter
 """ -------------------- GLOBAL VARIABLES --------------------"""
 
 IS_RUNNING = True
+processMonitor = ProcessMonitor()
 
 """ -------------------- GLOBAL VARIABLES --------------------"""
 
@@ -18,7 +19,7 @@ IS_RUNNING = True
 if __name__ == "__main__":
     while IS_RUNNING:
         t1 = perf_counter()
-        data = getProcessesWithParent()
+        data = processMonitor.get_processes_with_parents()
         dump_data(data)
         t2 = perf_counter()
         print(f"{t2-t1:.3f} seconds")
