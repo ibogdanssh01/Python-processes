db9afe4be90ce75b59cc2abdadb37b94603ed6a8
ibogdanssh01 <ibogdanssh@gmail.com>
2025-07-04 09:07:52 +0300

    Update python_processes, a clean version of it and creating a new file that monitor our processes and calculate the exact time of it

--- src/core/python_processes/python_processes.py
+++ src/core/python_processes/python_processes.py
@@ -5,6 +5,8 @@ sys.path.append('../')
 from src.core.python_processes.enums import ProcessType
 from src.config.json_script import rule_loader
 import psutil
+from datetime import timedelta
+import time
 
 """ --------------------- GLOBAL VAR -----------------"""
 
@@ -56,59 +58,3 @@ class ProcessMonitor:
                 return ProcessType.PROCESS_CATEGORY_SYSTEM
 
         return ProcessType.PROCESS_CATEGORY_APP
-
-# ! TEST PURPOSE ONLY
-# def display_process_tree(process_dict, indent=0):
-#     for parent_pid, children in process_dict.items():
-#         print("    " * indent + f"{parent_pid}: {{")
-#         for child_pid, info in children.items():
-#             print("    " * (indent + 1) + f"{child_pid}: {{")
-#             for key, value in info.items():
-#                 print("    " * (indent + 2) + f'"{key}": {repr(value)}')
-#             print("    " * (indent + 1) + "},")
-#         print("    " * indent + "},")
-
-
-# ! TEST PURPOSE ONLY -> TODO: In this moment, in near future after a few optimization can be processes like a new feature
-# def monitor_system_to_json(log_interval=2, duration=10, output_path='./output/process_log.json'):
-#     log_data = []
-#     start_time = time.time()
-
-#     while time.time() - start_time < duration:
-#         timestamp = datetime.now().isformat()
-#         snapshot = []
-
-#         for proc in psutil.process_iter(['pid', 'name','cpu_percent', 'memory_info']):
-#             try:
-#                 cpu = proc.cpu_percent(interval=0)
-#                 mem = proc.info['memory_info'].rss / (1024 * 1024) # MB
-#                 snapshot.append({
-#                     'pid': proc.info['pid'],
-#                     'name': proc.info['name'],
-#                     'cpu_percent': round(cpu, 2),
-#                     'memory_mb': round(mem, 2)
-#                 })
-#             except (psutil.NoSuchProcess, psutil.AccessDenied):
-#                 continue
-#         log_data.append({
-#             'timestamp': timestamp,
-#             'processes': snapshot
-#         })
-
-#         time.sleep(log_interval)
-#         with open(output_path, 'wb') as f:
-#             f.write(orjson.dumps(log_data))
-
-
-
-# def get_process_category(path: str) -> ProcessType:
-#     if not path:
-#         return ProcessType.PROCESS_CATEGORY_SYSTEM
-
-#     path = path.lower()
-
-#     for sys_path in rules["system_paths"]:
-#         if sys_path in path:
-#             return ProcessType.PROCESS_CATEGORY_SYSTEM
-
-#     return ProcessType.PROCESS_CATEGORY_APP
